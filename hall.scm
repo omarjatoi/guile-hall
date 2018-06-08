430e0cf75f2eba16dc0090e171e48346a8691864
Alex Sassmannshausen <alex@pompo.co>
2018-06-08 14:15:19 +0200

    Update Hall file.
    * hall.scm: Update with our files.

--- hall.scm
+++ hall.scm
@@ -11,9 +11,19 @@
  (dependencies
   `(("guix" ,guix) ("guile-config" ,guile-config)))
  (files (libraries
-         ((scheme-file "hall") (directory "hall" ())))
+         ((directory
+           "hall"
+           ((scheme-file "builders")
+            (scheme-file "scan")
+            (scheme-file "spec")
+            (scheme-file "common")
+            (scheme-file "dist")
+            (scheme-file "clean")
+            (scheme-file "init")
+            (scheme-file "guix")))))
         (tests ((directory "tests" ())))
-        (programs ((directory "scripts" ())))
+        (programs
+         ((directory "scripts" ((in-file "hall")))))
         (documentation
          ((text-file "README")
           (text-file "HACKING")
