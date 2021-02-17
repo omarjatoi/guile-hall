be4c0f9717dd40b3d6e74ab2b492cbbec5b1bddb
Alex Sassmannshausen <alex@komputilo.eu>
2021-02-17 17:04:55 +0100

    Regenerate hall & Makefile.

--- hall.scm
+++ hall.scm
@@ -1,38 +1,40 @@
 (hall-description
- (name "hall")
- (prefix "guile")
- (version "0.3.1")
- (author "Alex Sassmannshausen")
- (copyright (2018 2020))
- (synopsis "Guile project tooling")
- (description "Hall is a command-line application and a set of Guile libraries that allow you to quickly create and publish Guile projects.  It allows you to transparently support the GNU build system, manage a project hierarchy & provides tight coupling to Guix.")
- (home-page "https://gitlab.com/a-sassmannshausen/guile-hall")
- (license gpl3+)
- (dependencies
-  `(("guile-config" (config) ,guile-config)))
- (files (libraries
-         ((directory
-           "hall"
-           ((scheme-file "builders")
-            (scheme-file "scan")
-            (scheme-file "spec")
-            (scheme-file "common")
-            (scheme-file "dist")
-            (scheme-file "clean")
-            (scheme-file "init")
-            (scheme-file "guix")
-            (scheme-file "workarounds")))))
-        (tests ((directory "tests" ())))
-        (programs
-         ((directory "scripts" ((in-file "hall")))))
-        (documentation
-         ((org-file "README")
-          (symlink "README" "README.org")
-          (text-file "HACKING")
-          (text-file "COPYING")
-          (text-file "NEWS")
-          (text-file "AUTHORS")
-          (text-file "ChangeLog")
-          (directory "doc" ((texi-file "hall")))))
-        (infrastructure
-         ((scheme-file "guix") (scheme-file "hall")))))
+  (name "hall")
+  (prefix "guile")
+  (version "0.3.1")
+  (author "Alex Sassmannshausen")
+  (copyright (2018 2020))
+  (synopsis "Guile project tooling")
+  (description
+    "Hall is a command-line application and a set of Guile libraries that allow you to quickly create and publish Guile projects.  It allows you to transparently support the GNU build system, manage a project hierarchy & provides tight coupling to Guix.")
+  (home-page
+    "https://gitlab.com/a-sassmannshausen/guile-hall")
+  (license gpl3+)
+  (dependencies
+    `(("guile-config" (config) ,guile-config)))
+  (files (libraries
+           ((directory
+              "hall"
+              ((scheme-file "builders")
+               (scheme-file "scan")
+               (scheme-file "spec")
+               (scheme-file "common")
+               (scheme-file "dist")
+               (scheme-file "clean")
+               (scheme-file "init")
+               (scheme-file "guix")
+               (scheme-file "workarounds")))))
+         (tests ((directory "tests" ((scheme-file "hall")))))
+         (programs
+           ((directory "scripts" ((in-file "hall")))))
+         (documentation
+           ((org-file "README")
+            (symlink "README" "README.org")
+            (text-file "HACKING")
+            (text-file "COPYING")
+            (text-file "NEWS")
+            (text-file "AUTHORS")
+            (text-file "ChangeLog")
+            (directory "doc" ((texi-file "hall")))))
+         (infrastructure
+           ((scheme-file "guix") (scheme-file "hall")))))
